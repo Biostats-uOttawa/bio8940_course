fccb94c51b42afe0a87e6e708d7a183af11884cc
JulienGAMartin <julien.martin@uottawa.ca>
2024-02-04 19:36:56 -0500

    Built site for gh-pages

--- slides/_purl_script.R
+++ slides/_purl_script.R
@@ -1,11 +1,14 @@
 files <- list.files(pattern = ".[Rq]md$")
 
-lec_purl <- c(5)
+lec_purl <- c(4)
 
 for (file in files[lec_purl]) {
   knitr::purl(
     file,
     output = paste0("../data/R_code/", gsub(".[Rq]md$", ".R", file))
   )
-  pagedown::chrome_print(gsub(".[Rq]md$", ".html", file))
+  pagedown::chrome_print(
+    gsub(".[Rq]md$", ".html", file),
+    timeout = 120
+    )
 }
