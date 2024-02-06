31655b80b32c886966a7e2d572c8020f2f905583
JulienGAMartin <julien.martin@uottawa.ca>
2024-02-05 22:50:42 -0500

    Built site for gh-pages

--- slides/_purl_script.R
+++ slides/_purl_script.R
@@ -1,14 +1,20 @@
+purl_pdf <- function(files_list, purl = TRUE, pdf = TRUE) {
+  for (file in files_list) {
+    if (isTRUE(purl)) {
+      knitr::purl(
+        file,
+        output = paste0("../data/R_code/", gsub(".[Rq]md$", ".R", file))
+      )
+    }
+    if (isTRUE(pdf)) {
+      pagedown::chrome_print(
+        gsub(".[Rq]md$", ".html", file),
+        timeout = 120
+      )
+    }
+  }
+}
+
 files <- list.files(pattern = ".[Rq]md$")
 
-lec_purl <- c(4)
-
-for (file in files[lec_purl]) {
-  knitr::purl(
-    file,
-    output = paste0("../data/R_code/", gsub(".[Rq]md$", ".R", file))
-  )
-  pagedown::chrome_print(
-    gsub(".[Rq]md$", ".html", file),
-    timeout = 120
-    )
-}
+purl_pdf(files[5],purl=FALSE)
